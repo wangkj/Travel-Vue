f4d064a3a14a12e76512b0bd050652d5d71ec1c4
wangkj <529562814@qq.com>
2018-11-22 01:47:50 +0800

    1.0.1

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+// 1像素边框问题 1px太高了，多倍屏幕
+import fastClick from 'fastclick'
+// 300ms点击问题，点击延迟 npm install fasclick
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div> 
+        <div class="home">home</div>
+        <router-link to="/list" class="home">列表</router-link>
+    </div>
+   
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style>
+    .home{
+        font-size: 20px
+    }
+</style>
